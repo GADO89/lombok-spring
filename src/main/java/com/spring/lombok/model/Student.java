a103f71f96a22a2fbbb2c31b38ad8905ac1468cd
GADO89 <gadomahmoud377@gmail.com>
2024-01-01 05:37:04 +0200

    EqualsAndHashCode lombok@

--- src/main/java/com/spring/lombok/model/Student.java
+++ src/main/java/com/spring/lombok/model/Student.java
@@ -16,6 +16,21 @@ public class Student {
     private String address;
     private String age;
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this==obj)return true;
+        if (obj==null)return false;
+        if (this.getClass()!=this.getClass())return false;
+        Student student=(Student) obj;
+        return this.id== student.getId() && this.getName()==student.getName()
+                && this.getAddress() ==student.getAddress() && this.getAge()== student.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id * name.hashCode() * address.hashCode() * age.hashCode());
+    }
+
     /*@Override
     public String toString() {
         return "ID ="+id+" name ="+name+" address ="+address+" age ="+age;
